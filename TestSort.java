ac0b2cbb7207c5ce52f6b29ebf1a21e654d10a21
yexuerui <1229533411@qq.com>
2019-02-26 19:06:00 +0800

    TestSort.java版本初建

---
+++ TestSort.java
@@ -0,0 +1,16 @@
+package Algorithm.Test;
+
+import Algorithm.Sort;
+
+public class TestSort {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 1};
+
+        Sort.merge(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
