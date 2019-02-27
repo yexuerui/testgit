8f478a56b90fd29e71ebbe25927bcba4fa3c3e1b
yexuerui <1229533411@qq.com>
2019-02-27 12:37:46 +0800

    TestSort.java 5.0

--- TestSort.java
+++ TestSort.java
@@ -12,6 +12,7 @@ public class TestSort {
         Sort.merge(arr);
 	  i++;
 	  i--;
+	//start
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
