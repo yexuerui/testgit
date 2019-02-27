20ae44ccc82e66044c48f44a6fca594438ca18db
yexuerui <1229533411@qq.com>
2019-02-27 13:07:15 +0800

    t-1

--- TestSort.java
+++ TestSort.java
@@ -1,12 +1,13 @@
 package Algorithm.Test;
 
+
 import Algorithm.Sort;
 
 public class TestSort {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 1};
-
+	int[] a1=arr;
 
         Sort.merge(arr);
         for (int i = 0; i < arr.length; i++) {
