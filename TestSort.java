42cd076ba0a8965fe47a8be820a4dbf534fba68b
yexuerui <1229533411@qq.com>
2019-02-27 10:17:57 +0800

    TestSort.java 4.0修改

--- TestSort.java
+++ TestSort.java
@@ -8,6 +8,7 @@ public class TestSort {
         int[] arr = {1, 2, 1};
 	  int i=0;
 	  int[] a = {2, 3, 4};
+        a[0]=3;
         Sort.merge(arr);
 	  i++;
         for (int i = 0; i < arr.length; i++) {
