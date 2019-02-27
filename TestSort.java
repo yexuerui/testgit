3899a1714544048d6382aa3d46efdaa9df45df14
yexuerui <1229533411@qq.com>
2019-02-27 10:21:49 +0800

    TestSort.java 4.0修改

--- TestSort.java
+++ TestSort.java
@@ -10,6 +10,7 @@ public class TestSort {
 	  int[] a = {2, 3, 4};
         Sort.merge(arr);
 	  i++;
+	  i--;
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
